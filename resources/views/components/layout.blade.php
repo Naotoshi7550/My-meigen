f63be054b79d8a9793a2c11e01fe76999c9e790c
Sho-taro <sho96taro.biz@gmail.com>
2023-06-10 10:20:11 +0900

    layoutコンポーネントのpropsの初期値を追加

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,3 +1,7 @@
+@props([
+    'title' => "名言 by俺 | ??",
+    'script' => '',
+])
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -6,14 +10,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
     <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+
+    <!-- google fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Zen+Antique&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=DotGothic16&family=Hachi+Maru+Pop&family=Klee+One:wght@600&family=Reggae+One&family=RocknRoll+One&family=Zen+Antique&family=Zen+Kaku+Gothic+New:wght@700&display=swap" rel="stylesheet">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="/css/style.css">
-    @if (isset($script))
     {{ $script }}
-    @endif
 </head>
 <body>
     <div class="lg:w-3/5 lg:mx-auto">
